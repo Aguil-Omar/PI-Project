3c27fdf920dfe89a62fb1873f5b74e679fa7d03a
kthiriMohamedyoussef <Mohamedyoussef.Kthiri@esprit.tn>
2025-02-09 08:44:05 +0100

    second commit

---
+++ src/main/java/com/esprit/models/Adresse.java
@@ -0,0 +1,51 @@
+package com.esprit.models;
+
+public class Adresse {
+        private int id;
+        private int codePostal;
+        private String region;
+
+    public Adresse(int id, int codePostal, String region) {
+        this.id = id;
+        this.codePostal = codePostal;
+        this.region = region;
+    }
+
+    public Adresse(int codePostal, String region) {
+            this.codePostal = codePostal;
+            this.region = region;
+        }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(int codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    @Override
+    public String toString() {
+        return "Adresse{" +
+                "id=" + id +
+                ", codePostal=" + codePostal +
+                ", region='" + region + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/esprit/models/Role.java
@@ -0,0 +1,8 @@
+package com.esprit.models;
+
+public enum Role {
+
+    ADMIN,
+    ORGANISATEUR,
+    PARTICIPANT
+}
